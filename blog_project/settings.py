019f646d1ba8dd5286c3d7ebc5eca18f36b60653
Yanlong Sun <yanlongsun@Yanlong.local>
2023-01-06 21:46:20 -0800

    m

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
-STATIC_DIR = os.path.join(BASE_DIR, 'blog/static')
+STATIC_DIR = os.path.join(BASE_DIR, '/staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'blog/static/')
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
