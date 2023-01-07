161f6f4996d89a6a386bc2b19d65020540fc0082
Yanlong Sun <yanlongsun@Yanlong.local>
2023-01-06 19:09:08 -0800

    m

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'blog/static/')
 STATICFILES_DIRS = [STATIC_DIR]
 
 LOGIN_REDIRECT_URL = '/'
