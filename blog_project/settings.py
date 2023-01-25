52328e7828a9bd4f4428a8a3d64f6411bd613347
Yanlong Sun <yanlongsun@Yanlong.local>
2023-01-24 20:51:23 -0800

    update yml

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'django-insecure-v&2nhx4=2qic)rehbz741of1%*n9spf5pw_1p6_48w_civc%#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '54.149.214.152', 'yanlong-sun.com', '0.0.0.0']
 
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL= '/'
