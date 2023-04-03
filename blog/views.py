536b5a0b56cb9ad2398e8c789cebd1c3dfdd0ab4
Yanlong Sun <yanlongsun@Yanlong.local>
2023-04-03 12:29:34 -0700

    reorg path

--- blog/views.py
+++ blog/views.py
@@ -5,18 +5,22 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
-from django.views.generic import (TemplateView, ListView, DetailView, 
+from django.views.generic import (TemplateView, ListView, DetailView,
                                   CreateView, UpdateView, DeleteView)
 # Create your views here.
 
+
 class HomePage(TemplateView):
-    template_name = 'home.html'
+    template_name = 'homepage/home.html'
+
 
 class Experience(TemplateView):
-    template_name = 'experience.html'
+    template_name = 'homepage/experience.html'
+
 
 class Projects(TemplateView):
-    template_name = 'projects.html'
+    template_name = 'homepage/projects.html'
+
 
 class PostListView(ListView):
     model = Post
@@ -24,8 +28,10 @@ class PostListView(ListView):
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
+
 class PostDetailView(DetailView):
     model = Post
+
 
 class CreatePostView(LoginRequiredMixin, CreateView):
     login_url = '/login/'
@@ -33,31 +39,39 @@ class CreatePostView(LoginRequiredMixin, CreateView):
     form_class = PostForm
     model = Post
 
+
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
 
+
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
+
 
 class DraftListView(LoginRequiredMixin, ListView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_list.html'
     model = Post
+
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
 
 """
 comments
 """
+
+
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
 
 @login_required
 def add_comment_to_post(request, pk):
@@ -71,13 +85,15 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/comment_form.html', {'form':form})
+    return render(request, 'blog/comment_form.html', {'form': form})
+
 
 @login_required
 def comment_approved(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
 
 @login_required
 def comment_remove(request, pk):
